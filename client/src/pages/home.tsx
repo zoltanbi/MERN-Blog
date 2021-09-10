63228a984a500fc848c8ffc855780f3a46a4815a
Zoltanbi <zoltanbiro001@gmail.com>
2021-09-09 21:04:25 -0700

    imported navigation and header components to home page

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import IPageProps from "../interfaces/page";
+import {Container} from "reactstrap";
+import Navigation from '../components/Navigation'
+import Header from '../components/Header'
 
 const HomePage: React.FunctionComponent<IPageProps> = props => {
     return (
-        <div>
-            Home Page
-        </div>
+        <Container fluid classname="p-0">
+            <Navigation/>
+            <Header
+                title="Typescript Blog Website"
+                headline="Check out these community posts!"
+            />
+            <Container className="mt-5">
+                Blog stuff here ....
+            </Container>
+        </Container>
     )
 }
 
